5b4d0bfe828c6300f7c08798f8e547a63ec13108
Walker-Walker <awalker0@gmail.com>
2020-06-27 18:06:40 -0700

    decided to use template literals to append all the weather data to dom, also used two for loops: one for loop to iterate through 5 objects of the forcast api array, second for loop for iterating though an empty array of city names pulled from local storage, set variable for current date using moment , completed api function calls for now, may have to scope into my first api call and grab the weather icon for the current weather forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,49 +3,122 @@
 //  make input box for user City Selection
 // store city selection and display in a history column/row (bootstrap)
 // input city selection (queryselector) to grab requested information on city weather from weather api... (.... / + "var" /)
+// scope into object in json OpenWeather, and create a var for each weather data...
 // requested information being current day forecast and 5 day forecast
 // take responses from Server APi and display in a div card (bootstrap) for selected city current day weather forcast
 // take response and display in 5 div cards for 5 day forecast
+// use for loop to add 5 day forcast cards to dom
+//insert cityname into array (push)
+// array set to --> json.stringify--> local storage
+// grab from local storage--->json.parse
+// for loop to display each item...loop through city search history.length
+
+var citySearchHistory = [];
+
+
+var currentDate = moment().format("l");
+
 
 var cityButtonEl = document.querySelector("#city-button");
 
+
 var buttonClickHandler = function () {
   var userCityChoice = document.getElementById("city-query").value;
-  console.log(userCityChoice);
+  citySearchHistory.push(userCityChoice);
+  localStorage.setItem("citySearchHistory", JSON.stringify(citySearchHistory));
+  console.log("city search history from array: " + citySearchHistory);
+  console.log("user city choice is: " + userCityChoice);
   getWeather(userCityChoice);
 };
 
-// $(cityButton).click(function() {
-//     var userInput = $("#city-query").val();
-//     console.log(userInput);
-//     getWeather(userInput);
-// })
-// how to associate id of city to city name? so everytime user types NewYork-->ID is matched and then set to variable inside get weather function
+// getSearchHistory();
+
+function getSearchHistory() {
+  for (i = 0; i < citySearchHistory.length; i++) {
+    var cityNames = JSON.parse(
+      localStorage.getItem("citySearchHistory")
+    ); 
+    console.log("City Names from Local Storage: " + cityNames[i]);
+  
+  }
+  
+}
+
+
+// use template literals
 function getWeather(city) {
   fetch(
     "http://api.openweathermap.org/data/2.5/weather?q=" +
       city +
-      "+&appid=886705b4c1182eb1c69f28eb8c520e20"
-  ).then(function (response) {
-    response.json().then(function (data) {
-      console.log(data);
-    });
-  });
-}
-function FiveDayForcast(city) {}
+      "+&appid=24262cef714b3904324824a13e7d7b8d"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var cityName = data.name;
+      var temperature = data.main.temp;
+      var humidity = data.main.humidity;
+      var windSpeed = data.wind;
+      var latt = data.coord.lat;
+      var long = data.coord.lon;
 
-function uvIndex(long, latt) {}
+      const card = `
+     <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${currentDate}</h5>
+      <p class="card-text">The temperature is: ${temperature}</p>
+      <p id = "uv"> This is uv index </p>
+    
+    </div>
+  </div> `;
+
+      //appending
+      document.querySelector("#current-weather").innerHTML = card;
+
+      getSearchHistory();
+      fiveDayForcast(cityName);
+      uvIndex(latt, long);
+    });
+}
+
+// insert this into html to show weather icon src =  "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png
+// use moment + 1 to get dates for forcast cards..
+function fiveDayForcast(city) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=24262cef714b3904324824a13e7d7b8d&units=imperial`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // data.list[i]
+      for (var i = 0; i < 5; i++) {
+        
+        var temp = data.list[i].main.temp;
+        var humidity = data.list[i].main.humidity;
+        console.log("temp and humidity " + temp + " " + humidity);
+        console.log("icon name png " + data.list[i].weather[0].icon);
+      }
+    });
+}
+
+function uvIndex(latt, long) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/uvi?appid=24262cef714b3904324824a13e7d7b8d&lat=${latt}&lon=${long}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var uV = data.value;
+    });
+}
 
 cityButtonEl.addEventListener("click", buttonClickHandler);
 
-
-
-
-
-
-
-http://api.openweathermap.org/data/2.5/forecast?q=new%20york&appid=886705b4c1182eb1c69f28eb8c520e20&units=imperial
-
+// http://api.openweathermap.org/data/2.5/forecast?q=new%20york&appid=886705b4c1182eb1c69f28eb8c520e20&units=imperial
 
 // var object = {
 //   coord: {
